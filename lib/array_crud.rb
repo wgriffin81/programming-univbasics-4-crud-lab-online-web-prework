4ae5457038a696fce5203036cf2a4f0687857441
Bill Griffin <williamgriffin81@hotmail.com>
2019-10-15 20:33:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -64,5 +64,10 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
+  new_array = array
+  number = index_number
+  new_element = element
+  
+  new_array[number](new_element)
 
 end
